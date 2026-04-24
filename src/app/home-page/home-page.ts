cef92ff3b4150483fc14b6f51e219da89adbb25a
Joseph <josepharl11@gmail.com>
2026-04-24 15:21:43 -0400

    feat: Made the header sticky so that it scrolls with the user

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MapCard } from '../map-card/map-card';
 
